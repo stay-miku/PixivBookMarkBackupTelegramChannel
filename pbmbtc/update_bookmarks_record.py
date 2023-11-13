f9539d2b408124bec7c9441848fe62e8461d1d84
stay_miku <staymiku520@gmail.com>
2023-11-13 23:06:05 +0800

    bug

--- pbmbtc/update_bookmarks_record.py
+++ pbmbtc/update_bookmarks_record.py
@@ -15,90 +15,92 @@ logger = logging.getLogger("update bookmarks record")
 
 
 # 更新单个作品, illust为简略meta(bookmarks api传递的作品meta数据) update_meta为是否强制更新meta
-async def update_single(illust, update_meta, session: sqlalchemy.orm.Session):
+async def update_single(illust, update_meta):
     illust_id = str(illust['id'])
     user_id = int(illust['userId'])
-    exists = session.query(db.Illust).filter_by(id=illust_id).first()
 
-    # 存在记录
-    if exists:
-        # 失效作品
-        if user_id == 0:
-            logger.debug(f"unavailable illust: {illust_id}")
-            exists.unavailable = 1
-            exists.queried = 1
-            # session.commit()
-        else:
-            # 更新meta 或 作品从失效到有效(这种情况只有作者把私有设为公开才行了)
-            if update_meta or exists.unavailable == 1:
-                logger.debug(f"update meta: {illust_id}")
-                meta = await retry(pixiv.get_illust_meta, 5, 0, pid=illust_id, cookie=config.cookie)
-                exists.title = meta['illustTitle']
-                exists.type = meta['illustType']
-                exists.comment = meta['illustComment']
-                exists.tags = format_tags(pixiv.get_tags(meta))
-                exists.upload_date = meta['uploadDate']
-                exists.user_id = meta['userId']
-                exists.user_name = meta['userName']
-                exists.user_account = meta['userAccount']
-                exists.page_count = meta['pageCount']
-                exists.ai = meta['aiType']
-                exists.detail = json.dumps(meta, ensure_ascii=False)
-                exists.unavailable = 0
+    with db.start_session() as session:
+        exists = session.query(db.Illust).filter_by(id=illust_id).first()
+
+        # 存在记录
+        if exists:
+            # 失效作品
+            if user_id == 0:
+                logger.debug(f"unavailable illust: {illust_id}")
+                exists.unavailable = 1
                 exists.queried = 1
                 # session.commit()
-            # 不更新meta
             else:
-                logger.debug(f"don't update: {illust_id}")
-                exists.queried = 1
-                # session.commit()
+                # 更新meta 或 作品从失效到有效(这种情况只有作者把私有设为公开才行了)
+                if update_meta or exists.unavailable == 1:
+                    logger.debug(f"update meta: {illust_id}")
+                    meta = await retry(pixiv.get_illust_meta, 5, 0, pid=illust_id, cookie=config.cookie)
+                    exists.title = meta['illustTitle']
+                    exists.type = meta['illustType']
+                    exists.comment = meta['illustComment']
+                    exists.tags = format_tags(pixiv.get_tags(meta))
+                    exists.upload_date = meta['uploadDate']
+                    exists.user_id = meta['userId']
+                    exists.user_name = meta['userName']
+                    exists.user_account = meta['userAccount']
+                    exists.page_count = meta['pageCount']
+                    exists.ai = meta['aiType']
+                    exists.detail = json.dumps(meta, ensure_ascii=False)
+                    exists.unavailable = 0
+                    exists.queried = 1
+                    # session.commit()
+                # 不更新meta
+                else:
+                    logger.debug(f"don't update: {illust_id}")
+                    exists.queried = 1
+                    # session.commit()
 
-    # 记录不存在
-    else:
-        # 失效作品
-        if user_id == 0:
-            logger.debug(f"unavailable and not saved: {illust_id}")
-            i = db.Illust()
-            i.id = illust_id
-            i.title = ""
-            i.type = illust['illustType']
-            i.comment = ""
-            i.tags = ""
-            i.upload_date = ""
-            i.user_id = ""
-            i.user_name = ""
-            i.user_account = ""
-            i.page_count = 0
-            i.ai = 0
-            i.detail = json.dumps(illust, ensure_ascii=False)
-            i.backup = 0
-            i.unavailable = 1
-            i.saved = 0
-            i.queried = 1
-            session.add(i)
-            # session.commit()
+        # 记录不存在
         else:
-            logger.debug(f"need backup: {illust_id}")
-            meta = await retry(pixiv.get_illust_meta, 5, 0, cookie=config.cookie, pid=illust_id)
-            i = db.Illust()
-            i.id = illust_id
-            i.title = meta['illustTitle']
-            i.type = meta['illustType']
-            i.comment = meta['illustComment']
-            i.tags = format_tags(pixiv.get_tags(meta))
-            i.upload_date = meta['uploadDate']
-            i.user_id = meta['userId']
-            i.user_name = meta['userName']
-            i.user_account = meta['userAccount']
-            i.page_count = meta['pageCount']
-            i.ai = meta['aiType']
-            i.detail = json.dumps(meta, ensure_ascii=False)
-            i.backup = 0
-            i.unavailable = 0
-            i.saved = 0
-            i.queried = 1
-            session.add(i)
-            # session.commit()
+            # 失效作品
+            if user_id == 0:
+                logger.debug(f"unavailable and not saved: {illust_id}")
+                i = db.Illust()
+                i.id = illust_id
+                i.title = ""
+                i.type = illust['illustType']
+                i.comment = ""
+                i.tags = ""
+                i.upload_date = ""
+                i.user_id = ""
+                i.user_name = ""
+                i.user_account = ""
+                i.page_count = 0
+                i.ai = 0
+                i.detail = json.dumps(illust, ensure_ascii=False)
+                i.backup = 0
+                i.unavailable = 1
+                i.saved = 0
+                i.queried = 1
+                session.add(i)
+                # session.commit()
+            else:
+                logger.debug(f"need backup: {illust_id}")
+                meta = await retry(pixiv.get_illust_meta, 5, 0, cookie=config.cookie, pid=illust_id)
+                i = db.Illust()
+                i.id = illust_id
+                i.title = meta['illustTitle']
+                i.type = meta['illustType']
+                i.comment = meta['illustComment']
+                i.tags = format_tags(pixiv.get_tags(meta))
+                i.upload_date = meta['uploadDate']
+                i.user_id = meta['userId']
+                i.user_name = meta['userName']
+                i.user_account = meta['userAccount']
+                i.page_count = meta['pageCount']
+                i.ai = meta['aiType']
+                i.detail = json.dumps(meta, ensure_ascii=False)
+                i.backup = 0
+                i.unavailable = 0
+                i.saved = 0
+                i.queried = 1
+                session.add(i)
+                # session.commit()
 
 
 async def update(update_meta: bool, delay: int, context: ContextTypes.DEFAULT_TYPE):
@@ -108,15 +110,16 @@ async def update(update_meta: bool, delay: int, context: ContextTypes.DEFAULT_TY
 
     bookmarks = await retry(pixiv.get_bookmarks, 5, 0, cookie=config.cookie, user=user["userId"])
     logger.info(f"update, total illusts: {bookmarks['total']}")
-    with db.start_session() as session:
-        total = bookmarks['total']
-        i = 1
-        for illust in bookmarks["illust"]:
-            await update_single(illust, update_meta, session)
-            i += 1
-            logger.info(f"{i}/{total}, process: {i/total}")
-            time.sleep(delay)
 
+    total = bookmarks['total']
+    i = 1
+    for illust in bookmarks["illust"]:
+        await update_single(illust, update_meta)
+        i += 1
+        logger.info(f"{i}/{total}, process: {i/total}")
+        time.sleep(delay)
+
+    with db.start_session() as session:
         # 删除所有未被更新的作品
         unlike = session.query(db.Illust).filter_by(queried=0).all()
         for un in unlike:
