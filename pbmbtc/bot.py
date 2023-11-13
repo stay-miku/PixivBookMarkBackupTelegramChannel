f86f92cdcb82ebb8da38203c7bae668c579b97f4
stay_miku <staymiku520@gmail.com>
2023-11-13 20:25:37 +0800

    test

--- pbmbtc/bot.py
+++ pbmbtc/bot.py
@@ -29,9 +29,10 @@ from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandl
 logger = logging.getLogger("bot")
 
 logging.getLogger("httpx").setLevel(logging.WARNING)
-logging.getLogger("httpcore").setLevel(logging.WARNING)
-logging.getLogger("telegram").setLevel(logging.WARNING)
-logging.getLogger("apscheduler").setLevel(logging.INFO)
+# logging.getLogger("httpcore").setLevel(logging.WARNING)
+# logging.getLogger("telegram").setLevel(logging.WARNING)
+# logging.getLogger("apscheduler").setLevel(logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 
 do_stop_bot = False
